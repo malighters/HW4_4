bcd071467b800d4fbfde6b8787b42226b3ee58a2
USER <C:\Users\USER\AppData\Roaming\The Bat!>
2022-10-04 20:00:47 +0300

    Initial commit

---
+++ HW4_4/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+abstract class GeoObject
+{
+    protected readonly int X;
+    protected readonly int Y;
+    protected readonly string Name;
+    protected readonly string Description;
+
+
+    public virtual string GetInfo()
+    {
+        return X + ", " + Y + " " + Name + " " + Description;
+    }
+
+    protected GeoObject(int x, int y, string name, string description)
+    {
+        X = x; 
+        Y = y;
+        Name = name;
+        Description = description;
+    }
+
+}
+
+class River: GeoObject
+{
+    private readonly int _speed;
+    private readonly int _lenght;
+
+    public River(int x, int y, string name, string description, int speed, int lenght) 
+        : base(x, y, name, description)
+    {
+        _speed = speed;
+        _lenght = lenght;
+    }
+
+    public override string GetInfo()
+    {
+        return X + ", " + Y + " " + Name + " " + Description + " " + _speed + "cm/s " + _lenght + "km";
+    }
+}
+
+class Mountain: GeoObject
+{
+    private readonly string _highestPoint;
+
+    public Mountain(int x, int y, string name, string description, string highestPoint)
+        : base(x, y, name, description)
+    {
+        _highestPoint = highestPoint;
+    } 
+    public override string GetInfo()
+    {
+        return X + ", " + Y + " " + Name + " " + Description + " " + _highestPoint;
+    }
+}
+
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        GeoObject a = new Mountain(105, 256, "AA", "none", "asd");
+        GeoObject b = new River(1366, 245, "da", "none", 64, 186);
+        Console.WriteLine(a.GetInfo());
+        Console.WriteLine(b.GetInfo());
+    }
+}
---
+++ HW_4_4_Interface/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public interface IGeoObject
+{
+    string GetInfo();
+}
+
+class River : IGeoObject
+{
+    
+    private readonly int _x;
+    private readonly int _y;
+    private readonly string _name;
+    private readonly string _description;
+    private readonly int _speed;
+    private readonly int _lenght;
+    
+    public River(int x, int y, string name, string description, int speed, int lenght)
+    {
+        _x = x; 
+        _y = y;
+        _name = name;
+        _description = description;
+        _speed = speed;
+        _lenght = lenght;
+    }
+    
+    
+    public string GetInfo()
+    {
+        return _x + ", " + _y + " " + _name + " " + _description + " " + _speed + "cm/s " + _lenght + "km";
+    }
+}
+
+class Mountain : IGeoObject
+{
+    
+    private readonly int _x;
+    private readonly int _y;
+    private readonly string _name;
+    private readonly string _description;
+    private readonly string _highestPoint;
+    
+    public string GetInfo()
+    {
+        return _x + ", " + _y + " " + _name + " " + _description + " " + _highestPoint;
+    }
+    
+    public Mountain(int x, int y, string name, string description, string highestPoint)
+    {
+        _x = x; 
+        _y = y;
+        _name = name;
+        _description = description;
+        _highestPoint = highestPoint;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Mountain a = new Mountain(105, 256, "AA", "none", "asd");
+        River b = new River(1366, 245, "da", "none", 64, 186);
+        Console.WriteLine(a.GetInfo());
+        Console.WriteLine(b.GetInfo());
+        Console.ReadKey();
+    }
+}
+
